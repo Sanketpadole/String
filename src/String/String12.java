b14aa1775ad7f3ba3afba0921f665b5cf27e8ca5
unknown <sanketpadole@nimapinfotech.com>
2023-10-24 12:27:16 +0530

    dfsguk

---
+++ src/String/String12.java
@@ -0,0 +1,12 @@
+package String;
+
+public class String12 {
+
+	public static void main(String[] args) {
+		String str = "hello";
+		String str1 = str + "world";
+		System.out.println("string" + str);
+
+	}
+
+}
